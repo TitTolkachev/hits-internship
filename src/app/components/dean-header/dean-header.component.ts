e04cbca77377190bafc0f59833a316f28389e7d3
Maxim Toloknov <toloknovmv@indorsoft.ru>
2024-06-25 15:59:00 +0700

    кнопка выхода из аккаунта (для студента и деканата)

--- src/app/components/dean-header/dean-header.component.ts
+++ src/app/components/dean-header/dean-header.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../../services/user.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-dean-header',
   templateUrl: './dean-header.component.html',
   styleUrl: './dean-header.component.css'
 })
-export class DeanHeaderComponent {
+export class DeanHeaderComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private authService: AuthService) {
+  }
 
+  ngOnInit(): void {
+    this.closeExitModal()
+  }
+
+  openExitModal(): void {
+    const exitModal = document.getElementById('logoutModal');
+    if (exitModal) {
+      exitModal.style.display = 'block';
+    }
+  }
+
+  closeExitModal(): void {
+    const modal = document.getElementById('logoutModal');
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  }
+
+  logout(): void {
+    this.userService.logout().subscribe(result => {
+      this.authService.signOut()
+      this.router.navigateByUrl("").then();
+    })
+    this.closeExitModal();
+  }
 }
