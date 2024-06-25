0f532fa0be5a7c467d8df468793bac789cb8c517
Maxim Toloknov <toloknovmv@indorsoft.ru>
2024-06-25 15:33:38 +0700

    форма регистрации студента

--- src/app/pages/invite/invite.component.ts
+++ src/app/pages/invite/invite.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {StreamService} from "../../services/stream.service";
+import {StudentDto} from "../../models/student.dto";
 
 @Component({
   selector: 'app-invite',
   templateUrl: './invite.component.html',
   styleUrl: './invite.component.css'
 })
-export class InviteComponent {
+export class InviteComponent implements OnInit {
+  student = {
+    login: '',
+    password: '',
+    firstName: '',
+    lastName: ''
+  };
 
+  flow = {
+    name: '',
+    participants: 0
+  };
+
+  code: string | undefined;
+  isLoading = false;
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private streamService: StreamService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    const inviteCode = this.route.snapshot.paramMap.get('code');
+    this.code = inviteCode!!
+    this.getStreamInfo(inviteCode!!);
+  }
+
+  getStreamInfo(code: string): void {
+    this.streamService.getStreamByInviteCode(code).subscribe(data => {
+      this.flow.name = data.name;
+      this.flow.participants = data.peopleNum;
+    })
+  }
+
+  onSubmit(): void {
+    // Здесь обработка формы регистрации, например отправка данных на сервер
+    console.log(this.student);
+    this.isLoading = true;
+    const userData: StudentDto = {
+      login: this.student.login,
+      name: this.student.firstName,
+      password: this.student.password,
+      surname: this.student.lastName,
+    }
+    this.streamService.registerByInviteCode(this.code!!, userData).subscribe(responce => {
+      this.isLoading = false;
+      this.router.navigateByUrl("").then(r => {
+      })
+    }, error => {
+      this.isLoading = false;
+    })
+  }
 }
--- src/app/services/stream.service.ts
+++ src/app/services/stream.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {SERVER_URL} from "../constants";
 import {InviteLink} from "../models/inviteLink";
+import {StreamByInviteCodeDto} from "../models/streamByInviteCode.dto";
+import {StudentDto} from "../models/student.dto";
 
 @Injectable({
   providedIn: 'root'
@@ -19,4 +21,13 @@ export class StreamService {
   createInviteLink(streamName: string): Observable<InviteLink> {
     return this.http.post<InviteLink>(`${SERVER_URL}/stream/create/${streamName}`, "");
   }
+
+  getStreamByInviteCode(code: string): Observable<StreamByInviteCodeDto> {
+    return this.http.get<StreamByInviteCodeDto>(`${SERVER_URL}/stream/get/${code}`);
+  }
+
+  registerByInviteCode(code: string, userData: StudentDto): Observable<VoidFunction> {
+    return this.http.post<VoidFunction>(`${SERVER_URL}/stream/register/${code}`, userData);
+  }
+
 }
