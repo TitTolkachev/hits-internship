0ab70c244cdcb704b2256c4211f9596fbcc25fc0
Tit Tolkachev <tittolkachev@gmail.com>
2024-06-27 17:16:47 +0700

    Экран компании-партнера.

--- src/app/partner-detail/partner-detail.component.ts
+++ src/app/partner-detail/partner-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Modal } from 'bootstrap';
-import {EditablePartnerInfo} from "./model/editable-partner-info.model";
-import {PartnerFullInfo} from "./model/parner-full-info.model";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
+import {SERVER_URL} from "../constants";
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-partner-detail',
@@ -10,127 +13,169 @@ import {PartnerFullInfo} from "./model/parner-full-info.model";
   styleUrls: ['./partner-detail.component.css']
 })
 export class PartnerDetailComponent implements OnInit {
-  partner: EditablePartnerInfo | undefined;
-  editPartnerName: string | undefined;
-  editPartnerDescription: string | undefined;
-  editNextMeetingDate: string | undefined;
-  editCompanyContactEmail: string | undefined;
-  editCompanyContactTelegram: string | undefined;
-  editFacultyContactFirstName: string | undefined;
-  editFacultyContactLastName: string | undefined;
-  editStudentOffers: string | undefined;
-  editJobPositions: string | undefined;
-  editModal: Modal | undefined;
+  companyId: any;
+  company: any;
+  positions: any[] = [];
+  stacks: any[] = [];
+  newPosition: any = {
+    stackId: null,
+    positionNum: null
+  };
+  newStack: any = {
+    name: ''
+  };
+  editPosition: any = {
+    id: null,
+    stackId: null,
+    positionNum: null
+  };
+  editCompany: any = {
+    id: null,
+    name: '',
+    companyContact: '',
+    hitsContact: ''
+  };
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  }
 
   ngOnInit(): void {
-    const partnerId = this.route.snapshot.paramMap.get('id');
-    const mockPartners: PartnerFullInfo[] = [
-      {
-        id: '1',
-        name: 'Компания 1',
-        description: 'Описание компании 1',
-        nextMeetingDate: 1717238400000,
-        studentOffers: ['student1', 'student2'],
-        jobPositions: { 'Android-разработчик': 10, 'Frontend': 5, 'ML': 4 },
-        companyContact: { email: 'contact@company1.com', telegram: '@company1' },
-        facultyContact: { firstName: 'Иван', lastName: 'Иванов' }
-      },
-      {
-        id: '2',
-        name: 'Компания 2',
-        description: 'Описание компании 2',
-        nextMeetingDate: 1717324800000,
-        studentOffers: ['student3', 'student4'],
-        jobPositions: { 'Android-разработчик': 8, 'Frontend': 6, 'ML': 5 },
-        companyContact: { email: 'contact@company2.com', telegram: '@company2' },
-        facultyContact: { firstName: 'Пётр', lastName: 'Петров' }
-      },
-      {
-        id: '3',
-        name: 'Компания 3',
-        description: 'Описание компании 3',
-        nextMeetingDate: 1717411200000,
-        studentOffers: ['student5', 'student6'],
-        jobPositions: { 'Android-разработчик': 12, 'Frontend': 4, 'ML': 3 },
-        companyContact: { email: 'contact@company3.com', telegram: '@company3' },
-        facultyContact: { firstName: 'Сергей', lastName: 'Сергеев' }
-      }
-    ];
-    this.partner = mockPartners.find(p => p.id === partnerId);
-
-    setTimeout(() => {
-      const modalElement = document.getElementById('editPartnerModal');
-      if (modalElement) {
-        this.editModal = new Modal(modalElement);
-        console.log("Modal initialized");
-      } else {
-        console.error();
-      }
-    }, 0);
+    this.route.paramMap.pipe(
+      switchMap(params => {
+        this.companyId = Number(params.get('id'));
+        return this.getCompanyDetails(this.companyId);
+      })
+    ).subscribe(company => {
+      this.company = company;
+      this.getInternshipPositions(this.companyId).subscribe(positions => {
+        this.positions = positions;
+        this.getStacks().subscribe(stacks => {
+          this.stacks = stacks;
+          this.positions.forEach(position => {
+            const stack = this.stacks.find(s => s.id === position.stackId);
+            position.stackName = stack ? stack.name : 'Неизвестный стек';
+          });
+        });
+      });
+    });
   }
 
-  formatDate(epochMillis: number): string {
-    const date = new Date(epochMillis);
-    return date.toLocaleString();
+  getCompanyDetails(companyId: number): Observable<any> {
+    return this.http.get(`${SERVER_URL}/companies/get/${companyId}`);
   }
 
-  openEditModal(): void {
-    if (this.partner) {
-      this.editPartnerName = this.partner.name;
-      this.editPartnerDescription = this.partner.description;
-      this.editNextMeetingDate = this.partner.nextMeetingDate ? this.toDateTimeLocal(new Date(this.partner.nextMeetingDate)) : '';
-      this.editCompanyContactEmail = this.partner.companyContact?.email;
-      this.editCompanyContactTelegram = this.partner.companyContact?.telegram;
-      this.editFacultyContactFirstName = this.partner.facultyContact?.firstName;
-      this.editFacultyContactLastName = this.partner.facultyContact?.lastName;
-      this.editStudentOffers = this.partner.studentOffers?.join(', ');
-      this.editJobPositions = this.partner.jobPositions ? Object.entries(this.partner.jobPositions).map(([key, value]) => `${key}:${value}`).join(', ') : '';
-      this.editModal?.show();
-    } else {
-      console.error();
-    }
+  getInternshipPositions(companyId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${SERVER_URL}/companies/stack/get/${companyId}`);
   }
 
-  saveChanges(): void {
-    if (this.partner) {
-      if (this.editPartnerName != null) {
-        this.partner.name = this.editPartnerName;
-      }
-      if (this.editPartnerDescription != null) {
-        this.partner.description = this.editPartnerDescription;
-      }
-      // @ts-ignore
-      this["partner"]["nextMeetingDate"] = <number>this.editNextMeetingDate ? new Date(this.editNextMeetingDate).getTime() : undefined;
-      if (this.partner.companyContact) {
-        if (this.editCompanyContactEmail != null) {
-          this.partner.companyContact.email = this.editCompanyContactEmail;
-        }
-        if (this.editCompanyContactTelegram != null) {
-          this.partner.companyContact.telegram = this.editCompanyContactTelegram;
-        }
-      }
-      if (this.partner.facultyContact) {
-        if (this.editFacultyContactFirstName != null) {
-          this.partner.facultyContact.firstName = this.editFacultyContactFirstName;
-        }
-        if (this.editFacultyContactLastName != null) {
-          this.partner.facultyContact.lastName = this.editFacultyContactLastName;
-        }
-      }
-      // @ts-ignore
-      this.partner.studentOffers = this.editStudentOffers?.split(',').map(s => s.trim());
-      this.partner.jobPositions = this.editJobPositions ? Object.fromEntries(this.editJobPositions.split(',').map(item => item.split(':').map(i => i.trim()))) : {};
-      this.editModal?.hide();
-    } else {
-      console.error();
-    }
+  getStacks(): Observable<any[]> {
+    return this.http.get<any[]>(`${SERVER_URL}/stack/get`);
   }
 
-  toDateTimeLocal(date: Date): string {
-    const offset = date.getTimezoneOffset();
-    const localDate = new Date(date.getTime() - (offset*60*1000));
-    return localDate.toISOString().slice(0, 16);
+  openModal(): void {
+    const modalElement = document.getElementById('addPositionModal');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+
+  openAddStackModal(): void {
+    const modalElement = document.getElementById('addStackModal');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+
+  addPosition(): void {
+    const body = {
+      stackId: Number(this.newPosition.stackId),
+      positionNum: Number(this.newPosition.positionNum)
+    };
+    this.http.post(`${SERVER_URL}/companies/stack/create/${this.companyId}`, body).subscribe(() => {
+      // Обновить список позиций после добавления новой
+      this.getInternshipPositions(this.companyId).subscribe(positions => {
+        this.positions = positions;
+        this.positions.forEach(position => {
+          const stack = this.stacks.find(s => s.id === position.stackId);
+          position.stackName = stack ? stack.name : 'Неизвестный стек';
+        });
+      });
+    });
+    const modalElement = document.getElementById('addPositionModal');
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+  }
+
+  addStack(): void {
+    const body = {
+      name: this.newStack.name
+    };
+    this.http.post(`${SERVER_URL}/stack/create`, body).subscribe(() => {
+      this.getStacks().subscribe(stacks => {
+        this.stacks = stacks;
+      });
+    });
+    const modalElement = document.getElementById('addStackModal');
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+  }
+
+  openEditModal(position: any): void {
+    this.editPosition = {...position};
+    const modalElement = document.getElementById('editPositionModal');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+
+  savePosition(): void {
+    const body = {
+      stackId: this.editPosition.stackId,
+      positionNum: this.editPosition.positionNum
+    };
+    this.http.patch(`${SERVER_URL}/companies/stack/update/${this.editPosition.id}`, body).subscribe(() => {
+      // Обновить список позиций после сохранения изменений
+      this.getInternshipPositions(this.companyId).subscribe(positions => {
+        this.positions = positions;
+        this.positions.forEach(position => {
+          const stack = this.stacks.find(s => s.id === position.stackId);
+          position.stackName = stack ? stack.name : 'Неизвестный стек';
+        });
+      });
+    });
+    const modalElement = document.getElementById('editPositionModal');
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+  }
+
+  deletePosition(): void {
+    this.http.delete(`${SERVER_URL}/companies/stack/delete/${this.editPosition.id}`).subscribe(() => {
+      // Обновить список позиций после удаления
+      this.getInternshipPositions(this.companyId).subscribe(positions => {
+        this.positions = positions;
+        this.positions.forEach(position => {
+          const stack = this.stacks.find(s => s.id === position.stackId);
+          position.stackName = stack ? stack.name : 'Неизвестный стек';
+        });
+      });
+    });
+    const modalElement = document.getElementById('editPositionModal');
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+  }
+
+  openEditCompanyModal(): void {
+    this.editCompany = { ...this.company };
+    const modalElement = document.getElementById('editCompanyModal');
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+
+  updateCompany(): void {
+    this.http.put(`${SERVER_URL}/companies/update`, this.editCompany).subscribe(() => {
+      this.getCompanyDetails(this.companyId).subscribe(company => {
+        this.company = company;
+      });
+    });
+    const modalElement = document.getElementById('editCompanyModal');
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
   }
 }
